cee0f52402edc0b27700a878c87ac930f3b37464
Gabo1234567890 <gabonenov@gmail.com>
2026-01-20 03:17:36 +0200

    User data fetching combined and cleaned up

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { ParticipationsModule } from 'src/participations/participations.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), ParticipationsModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
