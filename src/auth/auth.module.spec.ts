8b32cebe6907aedfaecd8ff80f7679b28afcfc35
Gabo1234567890 <gabonenov@gmail.com>
2026-03-07 22:45:05 +0200

    unit test error

--- src/auth/auth.module.spec.ts
+++ src/auth/auth.module.spec.ts
@@ -3,5 +3,6 @@ import { AuthModule } from './auth.module';
 describe('AuthModule', () => {
   it('should compile', async () => {
     expect(AuthModule).toBeDefined();
+    expect(true).toBe(false);
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,5 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 5000, '0.0.0.0');
-
-  const broken = 
 }
 void bootstrap();
